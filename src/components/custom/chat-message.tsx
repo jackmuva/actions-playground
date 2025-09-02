688c442aa0b7f9ae65bb2d1727eb4f4d726d79e0
jackmuva <jackmu@umich.edu>
2025-09-01 23:02:08 -0700

    dropdowns for tool calls

--- src/components/custom/chat-message.tsx
+++ src/components/custom/chat-message.tsx
@@ -2,6 +2,7 @@ import { UIMessage } from "ai";
 import { useState } from "react";
 import ReactMarkdown from 'react-markdown';
 import style from "./markdown.module.css";
+import { ChevronDownIcon } from "lucide-react";
 
 export const ChatMessage = ({ message, part }:
 	{ message: UIMessage, part: any }) => {
@@ -27,13 +28,21 @@ export const ChatMessage = ({ message, part }:
 	} else if (part.type.substring(0, 4) === 'tool') {
 		return (
 			<div className={`p-2 m-1 rounded-md min-w-16 max-w-3/4 
-				flex flex-col cursor-pointer 
-				${expand ? "max-h-96" : "max-h-28"}`}
-				onClick={toggleExpand}>
-				<div className="font-semibold">calling tool...</div>
-				<pre className={`p-2 rounded-sm text-sm bg-background-muted/20 
+				flex flex-col ${expand ? "max-h-96" : "max-h-28"}`}>
+				<div className={`flex items-center rounded-t-sm w-fit p-1 
+						cursor-pointer underline text-indigo-700 hover:text-indigo-600`}
+					onClick={toggleExpand}>
+					<div className={expand ? "text-muted-foreground rotate-180" :
+						"text-muted-foreground"} >
+						<ChevronDownIcon className={``} size={20} />
+					</div>
+					<div className="font-semibold">
+						See Agent Using ActionKit
+					</div>
+				</div>
+				<pre className={`p-2 text-sm bg-background-muted/20 cursor-pointer rounded-sm 
 						font-mono ${expand ? "overflow-auto" :
-						"overflow-hidden"}`}>
+						"overflow-hidden"}`} onClick={toggleExpand}>
 					{JSON.stringify(part, null, 2)}
 				</pre>
 			</div>
